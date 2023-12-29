9c7155a220b4fdee47d09a08a543c23361da8f12
zouyos <hamza.benketaf@gmail.com>
2023-12-29 15:16:42 +0100

    cours udemy pre-video 64

--- src/components/ExpenseInput/ExpenseInput.jsx
+++ src/components/ExpenseInput/ExpenseInput.jsx
@@ -1,8 +1,20 @@
+import { addExpense } from "store/expense/expsense-slice";
 import s from "./style.module.css";
+import { useDispatch } from "react-redux";
 
 export function ExpenseInput(props) {
+  const dispatch = useDispatch();
+
+  function submit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const name = formData.get("name");
+    const price = formData.get("price");
+    dispatch(addExpense({ name, price }));
+  }
+
   return (
-    <form>
+    <form onSubmit={submit}>
       <div className="row justify-content-center">
         <div className="col-12 col-sm-5 col-md-4 col-lg-4 mb-2">
           <input
--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import "./global.css";
 import { App } from "./App";
-import React, { StrictMode } from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "store";
+//Quand on déclare seulement un dossier, ça va chercher directement l'index
+//NB: Grâce à jsconfig.json, plus besoin d'indiquer le chemin relatif, ça va chercher depuis src
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <StrictMode>
+  <Provider store={store}>
     <App />
-  </StrictMode>
+  </Provider>
 );
